689ddd569b0cc5bb49f0d2fc687c21e1088c6325
m7ace <mohammed.ali25011@coai.uobaghdad.edu.iq>
2026-07-17 17:24:23 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,13 @@
+import os
+
+TOKEN = os.getenv("TOKEN")
+
+BOT_NAME = "Ultra Downloader Bot"
+
+VERSION = "1.0"
+
+DOWNLOAD_FOLDER = "downloads"
+
+MAX_VIDEO_SIZE = 49 * 1024 * 1024
+
+MAX_AUDIO_SIZE = 49 * 1024 * 1024
